d2a0183c35ef945622a8e9b28d1e7eeba1ea994c
kevin berg <kevinberg@live.se>
2023-03-27 12:43:47 +0200

    Add project files.

---
+++ inlämingbank/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace inlämingbank.Pages
+{
+	public class PrivacyModel : PageModel
+	{
+		private readonly ILogger<PrivacyModel> _logger;
+
+		public PrivacyModel(ILogger<PrivacyModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
